6a14f8c72ff8826e193a1235b3b4517345650581
v-Kaefer <vitor.guttler@edu.pucrs.br>
2023-11-03 03:23:25 -0300

    Update Trab2Exec1.asm
    - end_string : ajustado pilha :+1:
    - words : ajustado pilha :+1:
    --> 'zera_pilha', para controle modular
    
    AJUSTANDO:
    - loop_string
    - inside_word
    - increment_word

--- Trab2Exec1.asm
+++ Trab2Exec1.asm
@@ -10,12 +10,9 @@ loop_string
     bez r1, r2, end_string  ; se r2 = r1 ('\0'), registra fim da string OK
     bez r4, r2, inside_word ; se r2 = (' '), vai p/ inside_word /////Possivelmente errado
 
-	//and r2, r4, r1
-	//bez r3, r4
-
-	BRANCH IF EQUAL:
-	sub r2, r2, r4
-	bez r0, r1, inside_word
+	;BRANCH IF EQUAL:
+	;sub r2, r2, r4
+	;bez r0, r1, inside_word
 
 
     ; É um espaço, mas estava dentro de uma palavra anteriormente
@@ -27,17 +24,22 @@ loop_string
 
 
 end_string
-    add r5, 1            ; r5 + 1 -> próxima string
-    ldi r1, 10           ; valor ASCII de nova linha | ASCII(10) = '\n'
-    stw r1, printchar    ; imprime nova linha
-    ldi r1, 48
+    add r5, 1           ; r5 + 1 -> próxima string
+	sub sp, 2			; Pilha (push)
+	stw r1, sp			; push(r1)
+    ldi r1, 10          ; valor ASCII de nova linha | ASCII(10) = '\n'
+    stw r1, printchar   ; imprime nova linha
+    ldw r1, sp			; pop (r1)
+	add sp, 2			; Pilha(pop)
+	add r0, 1			; PRÓXIMO CHAR
     bnz r7, words
 
 inside_word
-    ; incrementa o pointer(+1 letra) e pula p/ proximo 2° byte
-    add r3, r3, r5       ; r3 = r3 + r5 = 1 bit (espaço)
+    add r0, 1				; incrementa o pointer(+1 letra) e pula p/ proximo 2° byte
+	sub sp, 2
+	stw r0, sp				; push(r0)    
     
-    ;increment_word
+	;increment_word
 	sub sp, 2
 	stw r3, 2
 
@@ -48,25 +50,40 @@ inside_word
 
 
 words
-    sub r0, r5, 1         ; varholder = str-1
-    bez 0, r0, set_str1   ; if (r0 == 0) PC = set_str1
+	sub sp, 2			; Pilha(push)
+	stw r0, sp			; push(r0)
+	ldi r0, r5			; temporariamente preservar r5 (próx. string)
+	sub sp, 2			; Pilha(push)
+	stw r0, sp			; push2(r5)
+	ldi r0, r5
+	
+    sub r0, r5, 1       ; r0 = r5-1
+    bez 0, r0, set_str1 ; if (r0 == 0) PC = set_str1
 	sub 0, r5, 2
-    bez 0, r0, set_str2   ; if (r0 == 0) PC = set_str2
+    bez 0, r0, set_str2 ; if (r0 == 0) PC = set_str2
     sub 0, r5, 3
-    bez 0, r0, set_str3   ; if (r0 == 0) PC = set_str3
-    bez 4, r0, result	  ; começa display da resposta
+    bez 0, r0, set_str3 ; if (r0 == 0) PC = set_str3
+
+    bez 4, r0, result	; começa display da resposta
+
     bnz r7, lr
 
 set_str1
     stw r0,"Primeira string"
-	bnz r7, lr
+	bnz r7, zera_pilha
 set_str2
     stw r0,"Segunda string"
-	bnz r7, lr
+	bnz r7, zera_pilha
 set_str3
     stw r0,"Terceira string"
-	bnz r7, lr
+	bnz r7, zera_pilha
 
+zera_pilha
+	ldw r5, sp			; pop(r5)
+	add sp, 2			; Pilha(pop)
+	ldw at, sp
+	add sp, 2
+	bnz r7, lr
 
 result
 	start_result
