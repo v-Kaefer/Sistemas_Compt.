c064e8229256f0693656978f9d444d66fa0709e8
v-Kaefer <vitor.guttler@edu.pucrs.br>
2023-11-06 17:14:33 -0300

    Update Trab2Exec2.asm

--- Trab2Exec2.asm
+++ Trab2Exec2.asm
@@ -3,31 +3,30 @@ main
     ldi r0, select_vet    ; vetores
     ldi r1, 0       ; i & j = mantem igual e armazena na pilha como 1 variável
     ldi r3, 0       ; aux {vetx[i]}
-    bnz r7, insertion_sort
+    bez sp, insertion_sort
 
 insertion_sort
+    ldi sr, vet_ctrl
+    add sr, 1 ; Contagem dos vetores
+    slt sp, vet_control, 4
+    bnz sp, end
     ; insertion_sort(int * vec, int size)
-
-
-
     ; for
     ldi r2, r0        ; r2 = vetx[0]
     add r2, r2, r1    ; r2 = vetx[r1(i)]
     ldi r3, r2        ; aux = vet[r1(i)]
 
     ; r1 = j = i
-    bnz sp, loop_j
-    ; while ((j > 0) && (vec[j - 1] > aux)) { loop_j
-    
+    bez sp, loop_j    
 
 loop_print
 	ldw	r2,r0           ; r2 = r0 (vetor)
 	stw	r2,printint     ; printa vet[0]
 	ldi	r2,32           ; r2 = ""
 	stw	r2,printchar    ; printa ""
-	add	vet_control, 1            ; r4++
+	add	vet_ctrl, 1            ; r4++
 	add	r0,2            ; r0 + 2 (pula byte)
-	sub	sr,vet_size,vet_control ; sr = vet_size - vet_control=0
+	sub	sr,vet_size,vet_ctrl ; sr = vet_size - vet_control=0
 	bnz	sr,loop_print
 	ldi	r2,10           ; r2 = "\n"
 	stw	r2,printchar    ; print "\n"
@@ -41,23 +40,23 @@ loop_i
     ; for (i = 1; i < size; i++) {
     
     ; Se i > vet_size, termina loop
-	slt sr, r1, vet_control; if (r1(i/j)<vet_control) {sr=1} else {sr=0}
-    bez sr, fim_i       ; if (sr = 0) -> fim_i
+	slt sp, r1, vet_ctrl; if (r1(i/j)<vet_control) {sr=1} else {sr=0}
+    bez sp, fim_i       ; if (sr = 0) -> fim_i
 
     bnz sp, loop_j      ; if (sr = 1) -> loop_j
     
     
 fim_i
     add r1, r1, 1       ; r1++
-    slt sp, vet_control, r1
+    slt sp, r1, vet_ctrl
     bez sp, insertion_sort
     bnz sp, loop_i
 
 loop_j
     ; while ((j > 0) && (vet[j - 1] > aux)) {
     ; (j > 0) 
-    slt sr, r1, 0       ; if (r1(j) < 0) {sr=1} else {sr=0}
-    bnz sr, fim_j       ; if (sr(j) < 0) {loopend}
+    slt sp, r1, 0       ; if (r1(j) < 0) {sr=1} else {sr=0}
+    bnz sp, fim_j       ; if (sr(j) < 0) {loopend}
 
     ; (vet[j - 1] > aux)
 
@@ -68,8 +67,8 @@ loop_j
     ldi r4, sr
     sub r4, r4, 1       ; r4[j] = vetx[j - 1]    
 
-    slt sr, r4, r3      ; if (r4(vetx[j - 1 ] < aux )) {sr = 1} else {start_loop_j}
-    bnz sr, endloop_j   ; if (sr!=0) {endloop_j}
+    slt sp, r4, r3      ; if (r4(vetx[j - 1 ] < aux )) {sr = 1} else {start_loop_j}
+    bnz sp, endloop_j   ; if (sr!=0) {endloop_j}
 
     ; start_loop_j
     
@@ -106,15 +105,13 @@ end
 
 
 
-i 0
-j 0
-aux 0
+printint 0xf002
+printchar 0xf000
 first_print 4
 vet_size 5
-vet_control 0
+vet_ctrl 0
 next_vet 0
 vet1 10 5 3 8 1
 vet2 9 6 5 1
 vet3 9 10 2
-printint 0xf002
-printchar 0xf000
+
